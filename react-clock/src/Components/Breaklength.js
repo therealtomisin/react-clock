c5b1a0c5b93d6b7a1f475015ef9ebec346dff89e
therealtomisin <oluwatimilehineribake@gmail.com>
2020-11-30 13:23:32 +1000

    okay staged

--- react-clock/src/Components/Breaklength.js
+++ react-clock/src/Components/Breaklength.js
@@ -1,12 +1,15 @@
 import React from 'react'
 
-function Breaklength() {
+function Breaklength(props) {
     return (
         <>
             Break Length
             <div id = 'break-decrement'>
             
             </div>
+            <span id = 'break-length'>
+                {}
+            </span>
             <div id = 'break-increment'>
             <img src="https://img.icons8.com/officel/16/000000/circled-chevron-up.png"/>
             </div>
--- react-clock/src/Components/Sessionlength.js
+++ react-clock/src/Components/Sessionlength.js
@@ -1,12 +1,15 @@
 import React from 'react'
 
-function Sessionlength() {
+function Sessionlength(props) {
     return (
         <>
            Session Length
             <div id = 'session-decrement'>
                 
             </div> 
+            <span id = 'session-length'>
+                {}
+            </span>
             <div id = 'session-increment'>
             <img src="https://img.icons8.com/officel/16/000000/circled-chevron-up.png"/>
             </div>
