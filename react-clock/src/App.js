d5cba39520f378ab279f38ad021877de8a1cae74
therealtomisin <oluwatimilehineribake@gmail.com>
2020-12-03 11:20:03 +1000

    countdown works

--- react-clock/src/App.js
+++ react-clock/src/App.js
@@ -9,6 +9,9 @@ function App() {
   const [breakLength, setbreakLength] = useState(5)
   const [sessionLength, setSessionLength] = useState(25)
   const [seconds, setSeconds] = useState(60)
+  const [minutes, setMinutes] = useState(25)
+  const [disp, setDisp] = useState("00")
+  const [status, setStatus] = useState(false)
 
   const reduceBreakLength = () => {
     if (breakLength <= 1) return
@@ -27,21 +30,41 @@ function App() {
     setSessionLength((sessionLength) => sessionLength - 1)
   }
   const run = () => {
-    setInterval (countdown, 1000)
-  }
-  const xyz = (newVal) => {
-    setSeconds(newVal)
-    console.log(newVal)
+    setStatus(!status)
+    //let interval = setInterval (countdown, 1000)
+    if (status === false) {
+      clearInterval(setInterval (countdown, 1000))
+    }
+    else {
+      setInterval (countdown, 1000)
+    }
   }
   const countdown = () => {
-    console.log(seconds)
-    xyz((seconds) => seconds - 1)
-    
+      let kane;
+      setSeconds((seconds) => {
+        kane = seconds - 1
+        if (kane > 9){
+          return kane
+        }else{
+          return (`0${kane}`)
+        }
+      })
+      setDisp(kane)
+      if (kane === 0){
+        setMinutes((minutes)=> minutes - 1)
+        setSeconds(60)
+      }
+      if (kane === 60){
+        setDisp('00')
+      }
+      if (kane < 10){
+        setDisp(`0${kane}`)
+      }
   }
   // const reduceitem = (item) => {
   //   return item = item - 1
   // }
-
+  
   return (
     <div className="App">
       <header><h1>25 + 5 clock</h1></header>
@@ -55,7 +78,7 @@ function App() {
       </div>
       <div className = 'timer-sha'>
         <div className = 'timer-body'>
-          <Timerbody play = {run} seconds = {parseFloat(seconds)} timer = {sessionLength}/>
+          <Timerbody play = {run} seconds = {disp} timer = {minutes}/>
         </div>
       </div>
     </div>
